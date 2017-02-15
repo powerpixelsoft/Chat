38e9cacdc310a1985a5b21c0fafc3795f3c46575
George Vasilchenko <info@powerpixelsoft.net>
2017-02-15 12:36:11 +0100

    Intermediate changes

--- Chat.Server/Properties/AssemblyInfo.cs
+++ Chat.Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Chat.ServerProgram")]
+[assembly: AssemblyTitle("Chat.Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chat.ServerProgram")]
+[assembly: AssemblyProduct("Tcp Chat Server")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
